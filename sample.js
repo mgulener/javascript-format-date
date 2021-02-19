77860b1af41d29033b4300e1f972cf064a5f3488
Murat Gülener <muratgulener@gmail.com>
2021-02-19 10:30:31 +0300

    Update sample.js

--- sample.js
+++ sample.js
@@ -11,4 +11,16 @@ const formatDate = (date) => {
   return newDate.toLocaleDateString('tr-TR', options);
 }
 
+const formatTime = (date) => {
+  const newDate = new Date(date);
+  const options = { 
+    year: 'numeric', 
+    month: 'long', 
+    day: 'numeric',
+  };
+
+  return newDate.toLocaleTimeString('tr-TR', options);
+}
+
 formatDate(date); // 18 Şubat 2021
+formatTime(date); // 18 Şubat 2021 14:38:43
